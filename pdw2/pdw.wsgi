9fdd23671459a211d866d452cbaa7814644c8338
Marco Montanari <marco.montanari@gmail.com>
2013-12-27 20:29:23 +0100

    style and content updates

---
+++ pdw2/pdw.wsgi
@@ -0,0 +1,10 @@
+#set path to real path
+import os
+base_path = "/var/www/www.publicdomainworks.net/pdw"
+activate_this = os.path.join(base_path, 'bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
+from web import app as application
+
+import sys
+sys.path.insert(0, os.path.join(base_path, "pdw2')
--- pdw2/web.py
+++ pdw2/web.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify, render_template, json, request
-import urllib2, subprocess, os
+from flask import Flask, jsonify, render_template, json, request, current_app
+from functools import wraps
+import subprocess, os
 
 app = Flask(__name__)
 # genshi = Genshi(app)
 
 @app.route("/")
 def home():
-    # return render_template('index.html')
+    return render_template('index.html')
     return api_index()
 
 @app.route("/api")
@@ -14,7 +15,7 @@ def api_index():
     example = {
         "title": "An Example",
         "issued" : "19030101",
-        "authors" : [
+        "author" : [
             {
                 "name" : "John Smith",
                 "birth" : "1849",
@@ -22,41 +23,72 @@ def api_index():
         ]
     }
     example_json = json.dumps(example, indent=2)
-    example_query = 'jurisdiction=france&work=' + json.dumps(example)
+    example_query = 'jurisdiction=france&work=http://data.bnf.fr/15533097/2046___film/rdf.xml'
     return render_template('api.html', example=example_json,
             example_query=example_query)
 
+@app.route("/api/jurisdictions")
+def jurisdictions():
+    ret = {'valid':["france"]}
+    return jsonify(ret)
+
+
+def jsonp(func):
+    """Wraps JSONified output for JSONP requests."""
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            data = str(func(*args, **kwargs).data)
+            content = str(callback) + '(' + data + ')'
+            mimetype = 'application/javascript'
+            return current_app.response_class(content, mimetype=mimetype)
+        else:
+            return func(*args, **kwargs)
+    return decorated_function
+
+
 
 @app.route("/api/pd")
+@jsonp
 def api_pd():
-    # TODO: proper validation (e.g. with colander)
+    base_path = "/home/ya/okfn/pdcalc_new/pd/"
     if not 'jurisdiction' in request.args or not 'work' in request.args:
         return jsonify({
             'error': 'Missing jurisdiction or work parameter'
-            })
-    jurisdiction = request.args['jurisdiction']
-    workdata = json.loads(request.args['work'])
-    if not os.path.exists(os.path.join('pdcalc', jurisdiction)):
-        return jsonify({
-            'error': 'No calculator for that jurisdiction'
-            })
+        })
+    #jurisdiction + flavor combo
+    jd = request.args.get('jurisdiction')
+    jd = jd.split('/')
+    jurisdiction = jd[0]
+    flavor = None
+    if len(jd)>1:
+        flavor = jd[1]
     
-    try:
-        country_dir = 'pdcalc/%s/' % jurisdiction
-        maparg = country_dir+jurisdiction+'.map'
-        flowarg = country_dir+jurisdiction[0].upper() + jurisdiction[1:] +'.rdf'
-        open('pdcalc/pdw2tmp.json', 'w').write(json.dumps(workdata))
-        process = subprocess.Popen(['python', 'pdcalc/reasoner.py', maparg, flowarg, 'pdcalc/pdw2tmp.json'  ], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = process.communicate()
-        if err:
-            raise Exception(err)
-    except Exception, inst:
-        if app.debug:
-            raise
-        return jsonify({
-            'error': '%s' % inst
-            })
-    return jsonify({'out':out})
+    #work
+    work = request.args.get('work')
+    
+    #detail level
+    detail = request.args.get('detail', "low")
+
+    
+    cmd = ['python', os.path.join(base_path, "pdcalc.py")]
+    cmd.extend(['-o', "json"])
+    cmd.extend(['-f', "xml"])
+    
+    cmd.extend(['-d', detail])
+    cmd.extend(['-c', jurisdiction])
+
+    cmd.extend(['-i', work])
+    if flavor is not None:
+        cmd.extend(['-l', flavor])
+
+    process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    output = {}
+    output['output'] = out
+    output['error'] = err
+    return jsonify(output)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False, port=8001)
